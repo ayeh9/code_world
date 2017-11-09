37d9a9f82f9148a6be052998a1d1a1b08afc9e78
An Tran <33105716+antran415@users.noreply.github.com>
2017-11-09 12:17:10 -0800

    Add files via upload

---
+++ Grid.gd
@@ -0,0 +1,58 @@
+extends TileMap
+
+
+var tile_size = get_cell_size()
+var half_tile_size = tile_size/2
+
+enum ENTITY_TYPES {PLAYER}
+var grid_size = Vector2(16, 16)
+var grid = []
+var positions = []
+
+func _ready():
+	randomize()
+	for x in range(grid_size.x):
+		grid.append([])
+		for y in range(grid_size.y):
+			grid[x].append(null)
+	
+	var player = get_node("Player")
+	var beginning_pos = update_child_pos(player)
+	player.set_pos(beginning_pos)
+	for n in range(10):
+	#	var grid_pos = Vector2(randi() % int(grid_size.x), randi() % int(grid_size.y))
+	#	if not grid_pos in positions:
+	#		positions.append(grid_pos)
+	spawning(10)
+	
+func spawning(num):
+	#############
+	for i in range(num):
+		print(i)
+		for pos in positions:
+			print(pos, pos.y, pos.x)
+			var new_obstacle = get_node("Red")
+			new_obstacle.set_pos(map_to_world(pos) + half_tile_size)
+			grid[pos.x][pos.y] = new_obstacle
+			add_child(new_obstacle)
+	
+func is_cell_vacant(pos, direction):
+	var grid_pos = world_to_map(pos) + direction
+	
+	if grid_pos.x < grid_size.x and grid_pos.x >= 0:
+		if grid_pos.y < grid_size.y and grid_pos.y >= 0:
+			return true if grid[grid_pos.x][grid_pos.y] == null else false
+	return false
+	
+func update_child_pos(child_node):
+	var grid_pos = world_to_map(child_node.get_pos())
+	print(grid_pos)
+	grid[grid_pos.x][grid_pos.y] = null
+	
+	var new_grid_pos = grid_pos + child_node.direction
+	grid[new_grid_pos.x][new_grid_pos.y] = child_node.type
+	
+	var target_pos = map_to_world(new_grid_pos) + half_tile_size
+	return target_pos
+	pass
+
