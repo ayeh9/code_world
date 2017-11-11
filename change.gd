72800841937ea3410e18a7ff15e6e14c5a6c1073
An Tran <33105716+antran415@users.noreply.github.com>
2017-11-10 18:35:16 -0800

    Delete change.gd

--- change.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _ready():
-	connect("body_enter", self, "_enter_scene")
-
-func _enter_scene(body):
-	if(body.get_name() == "Player"):
-		get_tree().change_scene("res://Panel.tscn")
